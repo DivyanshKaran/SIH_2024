b0d7b9ec9c2ccfb764647c92abfe5c5905ad5d45
DivyanshKaran <karandivyansh01@gmail.com>
2024-09-03 17:06:38 +0530

    Added the main file uploader

---
+++ src/components/MainComponents/main.tsx
@@ -0,0 +1,14 @@
+import FileUploader from "../ui/FileUploader";
+
+function Main() {
+  return (
+    <>
+      <h1 className="mb-12">Welcome,Please upload your image here</h1>
+      <>
+        <FileUploader />
+      </>
+    </>
+  );
+}
+
+export default Main;
