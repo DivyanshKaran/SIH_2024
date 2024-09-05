4d574d8a7ebc88c4c238238fe95ca50e9457f1f1
DivyanshKaran <karandivyansh01@gmail.com>
2024-09-05 16:40:28 +0530

    Resolving branch conflicts

--- src/components/MainComponents/Main.tsx
+++ src/components/MainComponents/Main.tsx
@@ -29,22 +29,23 @@ function Main() {
   const fileRef = form.register("file");
   const onSubmit = (data: z.infer<typeof formSchema>) => {
     let formData = new FormData();
-    // console.log(data.file[0]);
+    console.log(data);
+    console.log(data.file[0]);
     formData.append("file", data.file[0]);
-    console.log(formData);
-    fetch("http://127.0.0.1:8000/predict", {
-      method: "POST",
-      body: formData,
-    })
-      .then((resp) => resp.json())
-      .then((data) => {
-        // if (data.errors) {
-        //   alert(data.errors);
-        // } else {
-        console.log(data);
-        setMarkup(data);
-        // }
-      });
+    // console.log(formData);
+    // fetch("http://127.0.0.1:8000/predict", {
+    //   method: "POST",
+    //   body: formData,
+    // })
+    //   .then((resp) => resp.json())
+    //   .then((data) => {
+    //     // if (data.errors) {
+    //     //   alert(data.errors);
+    //     // } else {
+    //     console.log(data);
+    //     setMarkup(data);
+    //     // }
+    //   });
   };
   return (
     <div className="flex flex-col content-center gap-12">
